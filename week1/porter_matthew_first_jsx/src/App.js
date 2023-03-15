b82b859689914c513a132d046ce955b2ff3069c1
Matt Porter <mattyp.ryan@gmail.com>
2023-03-15 11:43:36 -0700

    finished assignment

--- week1/porter_matthew_first_jsx/src/App.js
+++ week1/porter_matthew_first_jsx/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn UE5</li>
+        <li>Lab Sol's DustLoop</li>
+        <li>Git Gud</li>
+      </ul>
     </div>
   );
 }
