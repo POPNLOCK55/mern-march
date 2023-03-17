5fb57f4277ceb6fbff4647af3cc6e7ffe189a6a4
Matt Porter <mattyp.ryan@gmail.com>
2023-03-17 13:36:43 -0700

    More forms assignment

--- week1/porter_matthew_hook_form/src/components/Form.js
+++ week1/porter_matthew_hook_form/src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Form = (props) => {
+    // let bool = true
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
@@ -23,22 +24,42 @@ const Form = (props) => {
             <div>
                 <label>First Name: </label>
                 <input type="text" onChange={ (e) => setFirstName(e.target.value) }/>
-            </div>        
+                {firstName.length < 3 ?
+                <p>First name must be more than 2 characters!</p>:
+                null
+            }
+            </div>
             <div>
                 <label>Last Name: </label>
                 <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                {lastName.length < 3 ?
+                <p>Last name must be more than 2 characters!</p>:
+                null
+            }
             </div>
             <div>
                 <label>E-mail: </label>
                 <input type="email" onChange={ (e) => setEmail(e.target.value) } />
+                {email.includes("@") ?
+                null:
+                <p>Please enter a valid Email address</p>
+                }
             </div>
             <div>
                 <label>Password: </label>
                 <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                {password.length < 8 ?
+                <p>Password must be 8 characters or more!</p>:
+                null
+            }
             </div>
             <div>
                 <label>Confirm Password: </label>
                 <input type="text" onChange={ (e) => setConfPass(e.target.value) } />
+                {confPass !== password ?
+                <p>Passwords must match!</p>:
+                null
+            }
             </div>
             <input type="submit" value="Create User" />
         </form>
